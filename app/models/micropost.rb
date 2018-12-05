554a6e902a1c22d7b2c5d76f9300ff05d481c26d
BGin <bgin@intouchhealth.com>
2018-12-05 18:28:40 +0000

    Validate user and micropost.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# Validates Micropost
 class Micropost < ApplicationRecord
+  belongs_to :user
+  validates :content, length: { maximum: 140 },
+                      presence: true
 end
