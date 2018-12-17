29812c47304fd8460703a9e79fc32b477e5b3abb
BGin <bgin@intouchhealth.com>
2018-12-17 20:20:11 +0000

    comments

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,20 +1,20 @@
 # Module for constants.
 module Constants
-  # Maximum length of a micropost
+  # Maximum length of a micropost.
   MAX_STRING_LEN_MICROPOST = 140
 
-  # Maximum length of user name
+  # Maximum length of an email.
   MAX_STRING_LEN_EMAIL = 244
 
-  # Maximum length of user name
+  # Maximum length of user name.
   MAX_STRING_LEN_USER_NAME = 50
 
-  #Minimum length of password
+  # Minimum length of a password.
   MIN_STRING_LEN_PASSWORD = 6
 
   # Page title heading.
   PAGE_TITLE = 'Ruby on Rails Tutorial Sample App'
 
-  # Regex valid email
+  # Regex valid email.
   REGEX_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 end
